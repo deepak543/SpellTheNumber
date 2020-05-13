5a7ca547e85b5eb81c04fab8041eb9c3e08b5b11
deepakselvakumar <dd.deepak06@gmail.com>
2020-05-13 23:26:27 +0530

    added function to pick words for individual numbers from a static string array of words for numbers

--- src/main/java/com/assessment/javaassessment/NumberToWord.java
+++ src/main/java/com/assessment/javaassessment/NumberToWord.java
@@ -1,7 +1,51 @@
 package com.assessment.javaassessment;
 public class NumberToWord {
-    public static String spellNumber(long i) {
+    static String ones[] = {"", "One ", "Two ", "Three ", "Four ",
+            "Five ", "Six ", "Seven ", "Eight ",
+            "Nine ", "Ten ", "Eleven ", "Twelve ",
+            "Thirteen ", "Fourteen ", "Fifteen ",
+            "Sixteen ", "Seventeen ", "Eighteen ",
+            "Nineteen "
+    };
 
-        return "";
+
+    static String tens[] = {"", "", "Twenty ", "Thirty ", "Forty ",
+            "Fifty ", "Sixty ", "Seventy ", "Eighty ",
+            "Ninety "
+    };
+
+
+    static String convertNumberUnitToWord(int number, String s) {
+        StringBuilder word = new StringBuilder();
+        if (number > 19) {
+            word.append(tens[number / 10] + ones[number % 10]);
+        } else {
+            word.append(ones[number]);
+        }
+        if (number != 0) {
+            word.append(s);
+        }
+
+        return word.toString();
     }
+
+
+    public static String spellNumber(long number) {
+
+        StringBuilder numInWord = new StringBuilder();
+        numInWord.append(convertNumberUnitToWord((int) (number % 100), ""));
+
+        return numInWord.toString();
+    }
+
+
+    public static void main(String[] args) {
+
+        long number = 12;
+
+
+        System.out.println(spellNumber(number));
+
+    }
+
 }
--- src/test/java/com/assessment/javaassessment/NumberToWordTest.java
+++ src/test/java/com/assessment/javaassessment/NumberToWordTest.java
@@ -9,12 +9,18 @@ class NumberToWordTest {
     @Test
     public void tensTest()
     {
-        assertEquals(NumberToWord.spellNumber(13), "Thirteen");
+        assertEquals(NumberToWord.spellNumber(13).trim(), "Thirteen");
     }
 
     @Test
     public void hundredsTest()
     {
-        assertEquals(NumberToWord.spellNumber(120), "Hundred and thirty");
+        assertEquals(NumberToWord.spellNumber(120).trim(), "Hundred and Thirty");
+    }
+
+    @Test
+    public void thousandsTest()
+    {
+        assertEquals(NumberToWord.spellNumber(1200).trim(), "Thousand Two Hundred");
     }
 }
